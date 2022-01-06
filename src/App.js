2e49275910189b03597ae9ebaf68c86a9734b858
SabinaFi24 <sabinafi@gmail.com>
2022-01-06 19:46:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import * as React from "react";
+import {BrowserRouter} from "react-router-dom";
+import ProfilePage from "./ProfilePage";
+import StoresPage from "./StoresPage";
+import {Route} from "react-router";
+import NavigationBar from "./NavigationBar";
+import LoginPage from "./LoginPage";
+import Cookies from "universal-cookie";
+import FollowedPage from "./FollowedPage";
+import FollowedProfilePage from "./FollowedProfilePage";
+
+class App extends React.Component {
+
+    state = {
+        isLoggedIn: false,
+        token : "",
+        textFromWebsocket : ""
+
+    }
+
+    componentDidMount() {
+        const cookies = new Cookies();
+        if (cookies.get("logged_in")) {
+            this.setState({
+                isLoggedIn: true,
+                token : cookies.get("logged_in")
+            })
+        }
+        const ws = new WebSocket("ws://localhost:8989/stream?token=7791");
+        ws.onmessage = (message) => {
+            const data = JSON.parse(message.data);
+            this.setState({
+                textFromWebsocket : data.text
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    {
+                        this.state.isLoggedIn ?
+                            <div style={{display: "flex", alignItems: "start", marginTop: "50px"}}>
+                                <NavigationBar/>
+                                <Route path={"/"} component={ProfilePage} exact={true}/>
+                                <Route path={"/profile"} component={ProfilePage} exact={true}/>
+                                <Route path={"/stores"} component={StoresPage} exact={true}/>
+                                <Route path={"/freeSearch"} component={SearchPage} exact={true}/>
+                                <Route path={"/settings"} component={SettingsPage}/>
+                            </div>
+                            :
+                            <div>
+                                <Route path={"/"} component={LoginPage}/>
+                            </div>
+                    }
+                </BrowserRouter>
+                <div>
+                   Web socket : {this.state.textFromWebsocket}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default App;
